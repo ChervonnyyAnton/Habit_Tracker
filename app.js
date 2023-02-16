5fa2f4394b913120e3ad4642794c5659bcae2d93
CharvonnyyAnton <chervonnyy@icloud.com>
2023-02-16 19:11:27 +0100

    functions to arrowFunctions

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 /*
-	Пользователь хочет приобрести игру в магазине.
-	Он может это сделаь тольк если:
-	- Его баланс больше 1000 или число бонусов больше 100
-	- Он не забанен
-	- Игра не куплена
-	- Игра в продаже
-
-	Напишите условие для покупки и выведите в консоль результат
+	перевести обычную функцию в стрелочную
 */
 
-const balance = 1000;
-const bonus = 100;
+function toPowerFunction(num, power)
+{
+	const res = num ** power;
+	return res;
+}
 
-let myBalance = prompt('Введите ваш баланс');
-let myBonus = prompt('Введите количество бонусов');
-let isBanned = confirm('Вы забанены?');
-let isGamePurchased = confirm('Вы купили игру?');
-let isGameOnSale = confirm('Игра в продаже?');
-
-let canBuyGame = (myBalance >= balance || myBonus >= bonus) && !isBanned && !isGamePurchased && isGameOnSale;
-
-console.log(canBuyGame ? 'Вы можете купить игру' : 'Вы не можете купить игру');
+const toPowerArrow = (num, power) => num ** power;
