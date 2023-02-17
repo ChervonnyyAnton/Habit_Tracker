b8c2062178161326df26fbcc6194dc41b072aa4f
CharvonnyyAnton <chervonnyy@icloud.com>
2023-02-17 19:46:40 +0100

    ArrayManipulator

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
 /*
-	Пользователь:
-	- Возраст
-	- Наличие работы
-	- Деньги
-	Нужно проверить может ли он купить новый MacBook за 2000$?
-	Он может брать не только свои деньги, но и взять кредит.
-	Ему дадут 500$, только если ему больше 24-х лет и он
-	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
-	ином случае.
-	Напишите функцию, которая принимает данные пользователя
-	и товара и возвращает true или false;
+	Дан список задач
+	Сделать функции:
+		- добавить задачу в конец
+		- удаление задачи по названию
+		- перенос задачи в начало списка по названию
+
+	Всегда меняем исходный массив
 */
 
+const tasks = ["task 1"];
 
-const age = Number(prompt('Введите ваш возраст'));
-const hasJob = Boolean(confirm('Есть ли у вас работа?'));
-const availableMoney = Number(prompt('Сколько у вас денег?'));
-const targetPrice = Number(prompt('Сколько стоит товар?'));
-
-function getCredit(age, hasJob) 
-{
-	switch(true)
-	{
-		case age > 24 && hasJob:
-			return 500;
-		case age > 24:
-			return 100;
-		default:
-			return 0;
-	}
+// Добавить задачу в конец
+function Add(task) {
+  tasks.push(task);
+}
+// Удалить задачу по названию
+function Delete(task) {
+	if(!tasks.includes(task)) return console.log('Такой задачи нет');
+  const index = tasks.indexOf(task);
+  tasks.splice(index, 1);
+}
+// Перенести задачу в начало по названию
+function Prioritize(task) {
+	if (!tasks.includes(task)) return console.log("Такой задачи нет");
+  const index = tasks.indexOf(task);
+  const taskToMove = tasks.splice(index, 1);
+  tasks.unshift(taskToMove[0]);
 }
 
-const calculateMoney = (age, job, money, price) => 
-{
-	if(money >= price)
-	{
-		return money;
-	}
-
-	return money + getCredit(age, job);
-}
-
-const message = canAfford => canAfford ? "Вы можете купить товар" : "Вы не можете купить товар";
-
-console.log(message(calculateMoney(age, hasJob, availableMoney, targetPrice)));
+console.log(tasks);
+Add("task 2");
+console.log(tasks);
+Delete("task 1");
+console.log(tasks);
+Delete("false task")
+console.log(tasks);
+Prioritize("task 2");
+console.log(tasks);
+Prioritize("false task")
+console.log(tasks);
