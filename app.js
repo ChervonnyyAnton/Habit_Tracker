de52e63ff57e7bb694b0fc1741a5d950c3e8d72c
CharvonnyyAnton <chervonnyy@icloud.com>
2023-02-21 19:00:36 +0100

    I Love JS

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
 /*
-	Дан произвольный url
-    https://purpleschool.ru/course/javascript'
-    Нужно сделать финкиию, которая выводит в консоль:
-    Протокол (https)
-    Доменное имя (purpleschool.ru)
-    Путь внутри сайта (/course/javascript)
+	Задача вывести в консоль строку "Я люблю JS !" из массива,
+	проходя циклом в обратном порядке, не используя метод reverse.
 */
 
-const url = "https://purpleschool.ru/course/javascript";
-
-function parseUrl(url) {
-	if (typeof url !== "string") {
-		throw new Error("Url must be a string");
-	}
-	
-	if(!url.includes("://")) {
-		throw new Error("Url must contain protocol");
-	}
-
-	const firstBatch = url.split("://");
-	const protocol = firstBatch[0];
-
-	if(!protocol.includes("s")) {
-		console.log("Url must be secure");
-	}
-
-	const secondBatch = firstBatch[1].split("/");
-    const domain = secondBatch[0];
-
-	if(!domain.includes(".")) {
-		throw new Error("Url must contain domain");
-	}
-
-	if(!firstBatch[1].includes("/")) {
-		throw new Error("Url must contain path");
-	}
-
-	const path = "/" + secondBatch.slice(1).join("/");
-	return { protocol, domain, path };
+const arr = ['!','JS', 'люблю', 'Я'];
+const newArr = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+	newArr.push(arr[i]);
 }
 
-console.log(parseUrl(url));
+console.log(newArr.join(' '));
