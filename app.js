732745a619894078538e3aa3ebe3df6d8b8adad0
CharvonnyyAnton <chervonnyy@icloud.com>
2023-02-26 20:50:37 +0100

    Iteration through massive of massives

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-function power(pow) {
-    return function (num) {
-      return num**pow;
-    }
-}
+/*
+  Имеется массив изменения цен prices, где внутри
+  элемент массива является ценои в момент X,
+  2й - ценой в момент Y. Нужно преобразовать данные в массив, где будут отображены
+  только положительные изменения цен: [100, 150]
+*/
 
-const power = (pow) => (num) => num**pow;
+const prices = [
+  [100, 200],
+  [120, 100],
+  [200, 350],
+];
+
+const positivePrices = prices
+  .map(product => product[1] - product[0])
+  .filter(price => price > 0);
+
+console.log(positivePrices);
