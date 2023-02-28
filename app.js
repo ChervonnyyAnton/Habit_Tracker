7393b75daf40a197203ffd6f1afb85bb71d0153a
CharvonnyyAnton <chervonnyy@icloud.com>
2023-02-28 20:14:43 +0100

    extract substrings

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 /*
-  функция, которая возвращает true, если элемент есть в массиве и 
-  false, если элемент не найден.
+  вытащить имя из строки
 */
 
-const arr = [1, 4, 4, 5, 10];
-const exp = 5;
+const userName = "Вася aka Терминатор Пупкин";
 
-function some (array, value) {
-  return array.findIndex(el => el === value) >= 0;
+function findSpaceFrom(text, side) {
+  if(side.includes("front")){
+    return text.indexOf(" ");
+  }
+
+  if(side.includes("back")){
+    return text.lastIndexOf(" ");
+  }
+
+  return 0;
 }
 
-console.log(some(arr, exp));
-console.log(arr.some(el => el === exp));
+function extractFirstNameFrom(Text){
+  return Text.slice(0, findSpaceFrom(Text, "front"));
+}
+
+function extractLastNameFrom(Text){
+  return Text.slice(findSpaceFrom(Text, "back") + 1, Text.length);
+}
+
+const extractUserName = (userName) => `${extractFirstNameFrom(userName)} ${extractLastNameFrom(userName)}`
+
+console.log(`Имя: ${extractUserName(userName)}`);
