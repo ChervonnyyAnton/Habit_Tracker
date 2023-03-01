ee126bd576476a735586bffbff5c83106bd7eb9e
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-01 20:09:36 +0100

    ValidateNumber

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 /*
-  вытащить имя из строки
+  определить является ли номер действительным в РФ
 */
 
-const userName = "Вася aka Терминатор Пупкин";
+// верные
 
-function findSpaceFrom(text, side) {
-  if(side.includes("front")){
-    return text.indexOf(" ");
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7 (910) 3235356';
+const num4 = '+7 (910) 323-53-56';
+const num5 = '+7 (910) 323-53-56  ';
+
+// не верные
+
+const numError = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
+
+function isNumber(num){
+  return !isNaN(Number(num));
+}
+
+function startsWithEight(num){
+  return num.startsWith('8');
+}
+
+function cleanNum(num){
+  let noSpaces = num.replaceAll(' ', '');
+  let noLeftBrace = noSpaces.replaceAll('(', '');
+  let noRightBrace = noLeftBrace.replaceAll(')', '');
+  let clean = noRightBrace.replaceAll('-', '');
+  let changePlusSeven = clean.replaceAll('+7', 8);
+  return changePlusSeven;
+}
+
+function lengthIsOk(num){
+  return num.length === 11;
+}
+
+function isValid(num){
+  let cleanedNum = cleanNum(num);
+  return startsWithEight(cleanedNum) && lengthIsOk(cleanedNum) && isNumber(cleanedNum);
+}
+
+function validateNumbers(arr){
+  let newArray = new Array();
+  for(let i = 0; i < arr.length; i++){
+    let smallArray = new Array(arr[i], isValid(arr[i]));
+    newArray.push(smallArray);
   }
 
-  if(side.includes("back")){
-    return text.lastIndexOf(" ");
-  }
-
-  return 0;
+  return newArray;
 }
 
-function extractFirstNameFrom(Text){
-  return Text.slice(0, findSpaceFrom(Text, "front"));
-}
+const ArrayOfNumbers = new Array(num1, num2, num3, num4, num5, numError, num2Error, num3Error, num4Error);
 
-function extractLastNameFrom(Text){
-  return Text.slice(findSpaceFrom(Text, "back") + 1, Text.length);
-}
-
-const extractUserName = (userName) => `${extractFirstNameFrom(userName)} ${extractLastNameFrom(userName)}`
-
-console.log(`Имя: ${extractUserName(userName)}`);
+console.log(validateNumbers(ArrayOfNumbers));
