4dd6e540866d5c357fe785dd4b733b48bacc7fba
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-05 21:19:06 +0100

    sort array of objects

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 /*
-  замаскировать все, кроме последних 4х символов карты
+  отсортировать массив объектов
 */
 
-const card = "2342834503453353";
+const users = [
+{name:'Вася', age: 30 },
+{name:'Катя', age: 18 },
+{name:'Аня', аgе: 40 },
+{name: 'Петя', age: 25 }
+]
 
-function coverCardNumber(number){
-  let trimmedNumber = number.trim();
-  if(!isNaN(Number(trimmedNumber))){
-    return trimmedNumber
-      .slice(trimmedNumber.length - 4, trimmedNumber.length)
-      .padStart(trimmedNumber.length, '*');
-  }
-  else{
-    console.log("It is not a valid number");
-  }
-}
-
-console.log(coverCardNumber(card));
-
-//решение автора -_-
-console.log(card.slice(-4).padStart(card.length, '*'));
+console.log(users.sort((a, b) => a.age - b.age));
