f822c56db039715352a4bc742b2c252be9176b2c
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-07 21:51:17 +0100

    warehouse

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 /*
-  Релизовать методы увеличения и уменьшения баланса,
-  при котором каждая операция сохраняется в массив
-  operations в виде { reason: 'Оплата налогов', sum: -100 }. 
-  Возвращается true, если успешно и false, если не хватает баланса
-  Так же реализовать метод вывода числа операций по кошельку 
+  приложение - склад. Можно искать, добавлять и получать вес продукта
 */
 
-const wallet = {
-  balance: 0,
-  operations: [{reason: 'initial value', value: 0}],
-  addValue: function (reason, value){
-    if(Number(value) + this.balance < 0){
-      return 'Operation declined - not enough debit';
+const warehouse = {
+  goods: [{id: 0, weight: {kg: 0}, brand: 'nothing'}],
+  findGoodById: function (id) {
+    return this.goods.find(eg => eg.id == id);
+  },
+  addGood: function (good) {
+    if(this.findGoodById(good.id)){
+      return undefined;
     }
-    this.operations.push({reason: reason, value: Number(value)});
-    this.balance += value;
-    return `after adding ${value} for ${reason} the balance is ${this.balance}`;
+
+    this.goods.push(good);
   },
-  getOperations: function(){
-    return this.operations;
-  },
-  getBalance: function(){
-    return this.balance;
+  getWeightKg: function (good) {
+    return this.goods.reduce((acc, el) => 
+      acc += el?.weight?.kg ? el.weight.kg : 0,
+      0
+    )
   }
+};
+
+const car = {
+  id: 1,
+  weight: {
+    kg: 1000
+  },
+  brand: 'Ford'
 }
 
-console.log(wallet.getBalance());
-console.log(wallet.getOperations());
-console.log(wallet.addValue('positive context', 100));
-console.log(wallet.addValue("next positive context", 1100));
-console.log(wallet.addValue("positive negative context", -1000));
-console.log(wallet.addValue("negative context", -1100));
-console.log(wallet.getBalance());
-console.log(wallet.getOperations());
+const chair = {
+  id: 2,
+  weight: {
+    kg: 2
+  },
+}
+
+const paper = {
+  id: 3,
+  color: 'red'
+}
+
+
+console.log(warehouse.goods);
+warehouse.addGood(car);
+console.log(warehouse.findGoodById(car.id));
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.findGoodById(7));
+console.log(warehouse.getWeightKg());
