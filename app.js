f38bde1a1cd01a295346b47c03bad3bfa30bb591
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-10 21:08:58 +0100

    company structure

--- app.js
+++ app.js
@@ -1,53 +1,34 @@
 /*
-  приложение - склад. Можно искать, добавлять и получать вес продукта
+  дополнить объект методами для
+  получения названия компании,
+  ceo и сотрудника
 */
 
-const warehouse = {
-  goods: [{id: 0, weight: {kg: 0}, brand: 'nothing'}],
-  findGoodById: function (id) {
-    return this.goods.find(eg => eg.id == id);
-  },
-  addGood: function (good) {
-    if(this.findGoodById(good.id)){
-      return undefined;
+const company = {
+  name: 'ООО Агро',
+  employees: [
+    {
+      name: 'Света',
+      getName: function(){
+        return this.name;
+      }
     }
-
-    this.goods.push(good);
+  ],
+  ceo: {
+    name: 'Вася',
+    getName: function(){
+      return this.name;
+    }
   },
-  getWeightKg: function (good) {
-    return this.goods.reduce((acc, el) => 
-      acc += el?.weight?.kg ? el.weight.kg : 0,
-      0
-    )
+
+  getName: function(){
+    return this.name;
+  },
+  getEmployees: function(){
+    return this.employees.map(el => el.getName());
   }
-};
-
-const car = {
-  id: 1,
-  weight: {
-    kg: 1000
-  },
-  brand: 'Ford'
 }
 
-const chair = {
-  id: 2,
-  weight: {
-    kg: 2
-  },
-}
-
-const paper = {
-  id: 3,
-  color: 'red'
-}
-
-
-console.log(warehouse.goods);
-warehouse.addGood(car);
-console.log(warehouse.findGoodById(car.id));
-warehouse.addGood(car);
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-console.log(warehouse.findGoodById(7));
-console.log(warehouse.getWeightKg());
+console.log(company.getName());
+console.log(company.ceo.getName());
+console.log(company.getEmployees());
