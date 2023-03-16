b189a5f691f9627da230eb480c5f0ca2d442ebb3
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-16 07:45:17 +0100

    changeAtClick

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict'
 
-console.log(document);
+function changeAtClick() {
+	const input = document.querySelector(".input").value;
+    if (!input) {
+      return;
+    }
+
+    document.querySelector(".panel").innerText = input;
+    document.querySelector(".input").value = null;
+}
