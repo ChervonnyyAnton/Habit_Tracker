4fc174f21d385c47b484b8a89b155e7a2ffd72fa
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-17 07:33:00 +0100

    find elements in DOM

--- app.js
+++ app.js
@@ -20,5 +20,45 @@ function submit() {
     document.querySelector(".panel").innerText = input;
     document.querySelector(".input").value = null;
 	document.querySelector(".notification").classList.add('notification_active');
-	
 }
+
+function getElOne(){
+	const elementsCollection = document.getElementsByClassName("one");
+	for(let i = 0; i < elementsCollection.length; i++){
+		console.log(elementsCollection[i].textContent)
+	}
+}
+
+console.log(getElOne());
+
+function getElTwo(){
+	const element = document.getElementById("two");
+	return element.textContent;
+}
+
+console.log(getElTwo());
+
+function getElFour(){
+	const element = document.querySelector("[user-id = '4']");
+	return element.textContent;
+}
+
+console.log(getElFour());
+
+// решение junior
+
+console.log(document.querySelector('.one').innerText);
+console.log(document.querySelector('.one ~ div').innerText);
+console.log(document.querySelector('#two').innerText);
+console.log(document.querySelector('[user-id = "4"]').innerText);
+
+// middle+
+
+console.log(document.querySelectorAll('.one > span')[0].innerText);
+console.log(document.querySelectorAll(".one > span")[1].innerText);
+console.log(document.getElementsByClassName("one")[0].innerText);
+console.log(document.getElementsByClassName("one")[1].innerText);
+
+console.log(document.getElementById('two').innerText);
+
+console.log(document.querySelector('[user-id = "4"]').innerText);
