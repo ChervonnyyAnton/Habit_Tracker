2797255430bce427c309ade15984f11f53653f5e
CharvonnyyAnton <chervonnyy@icloud.com>
2023-03-19 19:46:17 +0100

    local storage logs

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 'use strict'
+localStorage.clear();
+
+function addLog() {
+  const data = localStorage.getItem("lastInput");
+  const lastInput = JSON.parse(data);
+  const newElement = document.createElement("div");
+  newElement.innerHTML = `<div>${lastInput.text}</div>`;
+  document.querySelector(".log").appendChild(newElement);
+}
 
 function onChange(e){
 	if(e.code != 'Enter'){
@@ -10,15 +19,15 @@ function onChange(e){
 
 function submit() {
 	const input = document.querySelector(".input").value;
+
     if (!input) {
 		document
 			.querySelector(".notification")
 			.setAttribute("class", "notification");
 		return;
     }
-}
 
-const obj = JSON.parse('{"a": 1}');
-console.log(obj);
-const str = JSON.stringify(obj);
-console.log(str);
+	const jsonString = JSON.stringify({ text: input });
+    localStorage.setItem("lastInput", jsonString);
+    addLog();
+}
